9fbaba6adba9465bda779789576935a3826db52c
Nourhan Khaled <elgendynour529@gmail.com>
2025-06-24 13:02:23 +0300

    add assets done

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,68 @@
+class AppAssets{
+
+  // app_logo
+  static const String logo ="assets/images/app_logo/logo.png";
+
+
+  // Sign up and Login
+  static const String successMark ="assets/images/Login&SignUp/Successmark.png";
+
+
+  // account_informations
+  static const String girl ="assets/images/account_informations/girl.png";
+  static const String arrow ="assets/images/account_informations/arrow.png";
+  static const String blurCircleBig ="assets/images/account_informations/blur_circle_big.png";
+  static const String blurCircleMedium ="assets/images/account_informations/blur_circle_medium.png";
+  static const String blurCircleSmall ="assets/images/account_informations/blur_circle_small.png";
+  static const String graph ="assets/images/account_informations/graph.png";
+
+
+  // main_screens
+  static const String profileImage ="assets/images/main_screens/profile_image.png";
+
+  // workout_screens
+  static const String v1 ="assets/images/workout_screens/1.png";
+  static const String v2 ="assets/images/workout_screens/2.png";
+  static const String v3 ="assets/images/workout_screens/3.png";
+  static const String v4 ="assets/images/workout_screens/4.png";
+  static const String v5 ="assets/images/workout_screens/5.png";
+  static const String v6 ="assets/images/workout_screens/6.png";
+  static const String v7 ="assets/images/workout_screens/7.png";
+  static const String v8 ="assets/images/workout_screens/8.png";
+  static const String v9 ="assets/images/workout_screens/9.png";
+  static const String fire ="assets/images/workout_screens/fire.png";
+  static const String line ="assets/images/workout_screens/line.png";
+  static const String map ="assets/images/workout_screens/map.png";
+  static const String mapRec ="assets/images/workout_screens/map_rec.png";
+  static const String rec1 ="assets/images/workout_screens/rec1.png";
+  static const String rec2 ="assets/images/workout_screens/rec2.png";
+  static const String rec3 ="assets/images/workout_screens/rec3.png";
+  static const String workoutRec ="assets/images/workout_screens/workout_rec.png";
+
+
+  // icons
+  static const String arrowBack ="assets/icons/arrow-back.svg";
+  static const String arrowForward ="assets/icons/arrow-forward.svg";
+  static const String arrowUpLong ="assets/icons/arrow-up-long.svg";
+  static const String arrowUp ="assets/icons/arrow_up.svg";
+  static const String calendar ="assets/icons/calendar.svg";
+  static const String check ="assets/icons/check.svg";
+  static const String checkCircle ="assets/icons/check-circle.svg";
+  static const String circle ="assets/icons/circle.svg";
+  static const String dumbbell ="assets/icons/dumbbell.svg";
+  static const String edit ="assets/icons/edit.svg";
+  static const String eyeClosed ="assets/icons/eye-closed.svg";
+  static const String eyeOpen ="assets/icons/eye-open.svg";
+  static const String filterList ="assets/icons/filter-list.svg";
+  static const String gridAlt ="assets/icons/grid-alt.svg";
+  static const String moon ="assets/icons/moon.svg";
+  static const String pause ="assets/icons/pause.svg";
+  static const String play ="assets/icons/play.svg";
+  static const String run ="assets/icons/run.svg";
+  static const String search ="assets/icons/search.svg";
+  static const String settings ="assets/icons/settings.svg";
+  static const String square ="assets/icons/square.svg";
+  static const String statistics ="assets/icons/statistics.svg";
+  static const String successVerified ="assets/icons/success_verified.svg";
+
+}
