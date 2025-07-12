0ec0b60ed05b6f0d3cd86e836fd61479908c627a
donia <doniaragab53@yahoo.com>
2025-07-12 22:24:55 +0300

    everything working

--- lib/ui/authentication/forgot_password/OTP_screen.dart
+++ lib/ui/authentication/forgot_password/OTP_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fitness2/l10n/app_localizations.dart';
+import 'package:fitness2/ui/authentication/forgot_password/create_new_password.dart';
 import 'package:fitness2/ui/custom_widget/arrow_back/arrow_back.dart';
 import 'package:fitness2/ui/custom_widget/button_container/button_container.dart';
 import 'package:fitness2/utils/app_colors.dart';
@@ -56,7 +57,7 @@ class OtpScreen extends StatelessWidget {
             ),
             SizedBox(height: height * 0.05),
             ButtonContainer(
-              onPressed: () {},
+              onPressed: () {Navigator.pushNamed(context, CreateNewPassword.routeName);},
               text: AppLocalizations.of(context)!.verify,
             ),
             Spacer(),
